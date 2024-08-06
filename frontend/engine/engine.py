bdc3abb7653cd15edac558e72f135ab3a326fb06
OpEnchanter <openchanter@gmail.com>
2024-08-06 14:22:18 -0400

    Added ui classes

--- frontend/engine/engine.py
+++ frontend/engine/engine.py
@@ -12,8 +12,9 @@ class sceneManager(object):
         self.scenes = {}
         self.currentScene = "none"
     def addScene(self, sceneName, sceneBootScript, sceneFrameScript):
+        self.currentScene = sceneName
         self.scenes[sceneName] = scene(sceneBootScript, sceneFrameScript) # Add scene to registry
-        self.scenes[sceneName].data["bootScript"]() # Run scene boot / init script
+        self.scenes[sceneName].data["bootScript"](self.scenes[self.currentScene]) # Run scene boot / init script
     def setScene(self, sceneName) -> bool:
         if sceneName in self.scenes.keys:
             self.currentScene = sceneName
@@ -22,5 +23,28 @@ class sceneManager(object):
     def removeScene(self, sceneName):
         self.scenes.pop(sceneName)
     def executeFrame(self):
-        if self.currentScene in self.scenes.keys:
-            self.scenes[self.currentScene].data["frameScript"]()
+        if self.currentScene in self.scenes.keys():
+            self.scenes[self.currentScene].data["frameScript"](self.scenes[self.currentScene])
+
+class uiElement(object):
+    def __init__(self, elementBootScript, elementFrameScript, sprite, position):
+        self.sprite = sprite
+        self.bootScript = elementBootScript
+        self.frameScript = elementFrameScript
+        self.position = position
+
+        elementBootScript(self)
+
+class uiManager(object):
+    def __init__(self, screen):
+        self.elements = []
+        self.screen = screen
+    def addElement(self, elementBootScript, elementFrameScript, sprite, position):
+        self.elements.append(uiElement(elementBootScript, elementFrameScript, sprite, position))
+    def removeElement(self, index):
+        self.elements.pop(index)
+    def executeFrame(self):
+        for elem in self.elements:
+            elem.frameScript(elem)
+            self.screen.blit(elem.sprite, elem.position)
+            
--- frontend/text/main.py
+++ frontend/text/main.py
@@ -1,21 +1,78 @@
 """Import project dependencies"""
 
 import pygame, json, sys, os
-origionalPath = list(sys.path)
+originalPath = list(sys.path)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import engine.engine as engine
-sys.path = origionalPath
+sys.path = originalPath
 
+
+"""Load Settings"""
 settings = {}
 
-with open("settings.json", "r") as settings_file:
+with open(os.path.join(os.path.dirname(__file__), "settings.json"), "r") as settings_file:
     settings = json.load(settings_file)
+
+
+"""Initialize Pygame For GUI"""
 
 pygame.init()
 
-win = pygame.display.set_mode((settings["screen"]["width"], settings["screen"]["height"]))
+win = pygame.display.set_mode((settings["screenConfig"]["width"], settings["screenConfig"]["height"]), pygame.RESIZABLE)
+
+pygame.display.set_caption("OpenSource Office Utilities Text")
+
+"""Scene Scripts"""
+def homeFrame(scene):
+    win.fill((
+        settings["appStyle"]["ui"]["color"]["backgroundColor"][0],
+        settings["appStyle"]["ui"]["color"]["backgroundColor"][1],
+        settings["appStyle"]["ui"]["color"]["backgroundColor"][2]
+    ))
+
+    pygame.draw.rect(
+        win, 
+        (
+            settings["appStyle"]["ui"]["color"]["tabsBarColor"][0],
+            settings["appStyle"]["ui"]["color"]["tabsBarColor"][1],
+            settings["appStyle"]["ui"]["color"]["tabsBarColor"][2]
+        ),
+        (
+            0, 
+            0, 
+            win.get_size()[0],
+            30
+        )
+    )
+
+    pygame.draw.rect(
+        win, 
+        (
+            settings["appStyle"]["ui"]["color"]["insetColor"][0],
+            settings["appStyle"]["ui"]["color"]["insetColor"][1],
+            settings["appStyle"]["ui"]["color"]["insetColor"][2]
+        ),
+        (
+            10, 
+            45, 
+            300,
+            550
+        ),
+        0,
+        0,
+        settings["appStyle"]["ui"]["transform"]["inset"]["topLeftRadius"],
+        settings["appStyle"]["ui"]["transform"]["inset"]["topRightRadius"],
+        settings["appStyle"]["ui"]["transform"]["inset"]["bottomLeftRadius"],
+        settings["appStyle"]["ui"]["transform"]["inset"]["bottomRightRadius"]
+
+    )
+
+def homeInit(scene):
+    pass
 
 globalSceneManager = engine.sceneManager()
+
+globalSceneManager.addScene("home", homeInit, homeFrame)
 
 while True:
     for event in pygame.event.get():
@@ -23,7 +80,8 @@ while True:
             pygame.quit()
             sys.exit()
     
-    win.fill((255, 255, 255))
+    win.fill((0, 255, 0))
 
+    globalSceneManager.executeFrame()
 
     pygame.display.flip()
